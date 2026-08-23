94c0a1a8cbf1bf0c8d5b597cd5945212e6eb8451
Gersi365 <Gersi365@outlook.com>
2026-08-23 18:59:15 +0200

    Enable pinned FFmpeg audio extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,7 @@ dependencies {
     implementation("androidx.media3:media3-exoplayer:$media3Version")
     implementation("androidx.media3:media3-exoplayer-hls:$media3Version")
     implementation("androidx.media3:media3-ui:$media3Version")
+    implementation("app.ownplay.ffmpeg:lib-decoder-ffmpeg-release:1.11.0@aar")
 
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
