ef5356203c47a635a47d821b56ec9a76b70f06d8
Gersi365 <Gersi365@outlook.com>
2026-08-24 16:24:50 +0200

    Fix Downloads progress import

--- app/src/main/java/app/ownplay/player/ui/DownloadsSettingsScreen.kt
+++ app/src/main/java/app/ownplay/player/ui/DownloadsSettingsScreen.kt
@@ -3,7 +3,6 @@ package app.ownplay.player.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.LinearProgressIndicator
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,7 +20,6 @@ import androidx.compose.material.icons.filled.Movie
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.VideoLibrary
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
