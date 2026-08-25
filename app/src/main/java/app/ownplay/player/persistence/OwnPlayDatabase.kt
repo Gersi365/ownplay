455d508bf50df16d2a1c34e53eeb2669d3e88ffa
Gersi365 <Gersi365@outlook.com>
2026-08-25 15:15:33 +0200

    Propagate Room invalidations across download worker instances

--- app/src/main/java/app/ownplay/player/persistence/OwnPlayDatabase.kt
+++ app/src/main/java/app/ownplay/player/persistence/OwnPlayDatabase.kt
@@ -66,6 +66,7 @@ abstract class OwnPlayDatabase : RoomDatabase() {
                 DATABASE_NAME,
             )
                 .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
+                .enableMultiInstanceInvalidation()
                 .build()
     }
 }
