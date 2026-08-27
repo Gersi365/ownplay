67dfdf015215d5efe278bcfa9cc402250c1474ea
Gersi365 <Gersi365@outlook.com>
2026-08-27 08:03:37 +0200

    Fix TV focus indication redraw import

--- app/src/main/java/app/ownplay/player/ui/tv/TvRemoteIndication.kt
+++ app/src/main/java/app/ownplay/player/ui/tv/TvRemoteIndication.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.drawscope.ContentDrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.node.DelegatableNode
 import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.invalidateDraw
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
