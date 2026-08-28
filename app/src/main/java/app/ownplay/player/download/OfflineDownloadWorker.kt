5110aba770aee66001ad35bc528e3fe411928f12
Gersi365 <Gersi365@outlook.com>
2026-08-28 16:26:19 +0200

    Block cross-scheme redirects for offline downloads

--- app/src/main/java/app/ownplay/player/download/OfflineDownloadWorker.kt
+++ app/src/main/java/app/ownplay/player/download/OfflineDownloadWorker.kt
@@ -573,6 +573,7 @@ class OfflineDownloadWorker(
         private val httpClient = OkHttpClient.Builder()
             .connectTimeout(20, TimeUnit.SECONDS)
             .readTimeout(45, TimeUnit.SECONDS)
+            .followSslRedirects(false)
             .build()
     }
 }
