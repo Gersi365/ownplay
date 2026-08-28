74cc4b371e87dfd97747496de8eab2df58864e6b
Gersi365 <Gersi365@outlook.com>
2026-08-28 09:04:47 +0200

    Cover bounded download retry budget

--- app/src/test/java/app/ownplay/player/download/OfflineDownloadRetryPolicyTest.kt
+++ app/src/test/java/app/ownplay/player/download/OfflineDownloadRetryPolicyTest.kt
@@ -18,4 +18,12 @@ class OfflineDownloadRetryPolicyTest {
             assertFalse("Expected $status to be permanent", isRetryableDownloadHttpStatus(status))
         }
     }
+
+    @Test
+    fun `automatic retry budget is bounded`() {
+        assertTrue(shouldRetryDownload(runAttemptCount = 0, retryableFailure = true))
+        assertTrue(shouldRetryDownload(runAttemptCount = 2, retryableFailure = true))
+        assertFalse(shouldRetryDownload(runAttemptCount = 3, retryableFailure = true))
+        assertFalse(shouldRetryDownload(runAttemptCount = 0, retryableFailure = false))
+    }
 }
