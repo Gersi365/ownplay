605d4c0df578c0ac70a630a198a818f7f89d3c5d
Gersi365 <Gersi365@outlook.com>
2026-08-28 09:04:31 +0200

    Bound automatic download retries

--- app/src/main/java/app/ownplay/player/download/OfflineDownloadRetryPolicy.kt
+++ app/src/main/java/app/ownplay/player/download/OfflineDownloadRetryPolicy.kt
@@ -1,6 +1,13 @@
 package app.ownplay.player.download
 
+internal const val MAX_AUTOMATIC_DOWNLOAD_RETRIES = 3
+
 internal fun isRetryableDownloadHttpStatus(statusCode: Int): Boolean =
     statusCode == 408 ||
         statusCode == 429 ||
         statusCode in 500..599
+
+internal fun shouldRetryDownload(
+    runAttemptCount: Int,
+    retryableFailure: Boolean,
+): Boolean = retryableFailure && runAttemptCount < MAX_AUTOMATIC_DOWNLOAD_RETRIES
