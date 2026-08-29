b6acd52d1c982ec9b62aabb23bfbcdab20442e17
Gersi365 <Gersi365@outlook.com>
2026-08-29 07:18:21 +0200

    Register additive sync schema version 6

--- app/src/main/java/app/ownplay/player/persistence/OwnPlayDatabase.kt
+++ app/src/main/java/app/ownplay/player/persistence/OwnPlayDatabase.kt
@@ -14,6 +14,13 @@ import app.ownplay.player.persistence.series.ProviderSeriesEntity
 import app.ownplay.player.persistence.series.ProviderSeriesEpisodeEntity
 import app.ownplay.player.persistence.series.ProviderSeriesSeasonEntity
 import app.ownplay.player.persistence.series.SeriesCatalogDao
+import app.ownplay.player.persistence.sync.DeviceSyncChannelEntity
+import app.ownplay.player.persistence.sync.DeviceSyncDao
+import app.ownplay.player.persistence.sync.DeviceSyncGroupEntity
+import app.ownplay.player.persistence.sync.DeviceSyncGroupMembershipEntity
+import app.ownplay.player.persistence.sync.DeviceSyncLocalStateEntity
+import app.ownplay.player.persistence.sync.DeviceSyncSourceEntity
+import app.ownplay.player.persistence.sync.MIGRATION_5_6
 import app.ownplay.player.persistence.vod.MediaFavoriteEntity
 import app.ownplay.player.persistence.vod.PlaybackProgressEntity
 import app.ownplay.player.persistence.vod.ProviderMovieEntity
@@ -41,8 +48,13 @@ import app.ownplay.player.persistence.vod.VodCatalogDao
         ProviderSeriesEntity::class,
         ProviderSeriesSeasonEntity::class,
         ProviderSeriesEpisodeEntity::class,
+        DeviceSyncLocalStateEntity::class,
+        DeviceSyncSourceEntity::class,
+        DeviceSyncChannelEntity::class,
+        DeviceSyncGroupEntity::class,
+        DeviceSyncGroupMembershipEntity::class,
     ],
-    version = 5,
+    version = 6,
     exportSchema = true,
 )
 abstract class OwnPlayDatabase : RoomDatabase() {
@@ -55,6 +67,7 @@ abstract class OwnPlayDatabase : RoomDatabase() {
     abstract fun vodCatalogDao(): VodCatalogDao
     abstract fun mediaDownloadDao(): MediaDownloadDao
     abstract fun seriesCatalogDao(): SeriesCatalogDao
+    abstract fun deviceSyncDao(): DeviceSyncDao
 
     companion object {
         const val DATABASE_NAME = "ownplay.db"
@@ -65,7 +78,13 @@ abstract class OwnPlayDatabase : RoomDatabase() {
                 OwnPlayDatabase::class.java,
                 DATABASE_NAME,
             )
-                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
+                .addMigrations(
+                    MIGRATION_1_2,
+                    MIGRATION_2_3,
+                    MIGRATION_3_4,
+                    MIGRATION_4_5,
+                    MIGRATION_5_6,
+                )
                 .enableMultiInstanceInvalidation()
                 .build()
     }
