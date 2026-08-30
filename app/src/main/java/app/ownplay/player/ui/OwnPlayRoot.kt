067f240393be424427c2f5026c8e5afffbfc730b
Gersi365 <Gersi365@outlook.com>
2026-08-30 08:58:24 +0200

    Select UI shell at build time

--- app/src/main/java/app/ownplay/player/ui/OwnPlayRoot.kt
+++ app/src/main/java/app/ownplay/player/ui/OwnPlayRoot.kt
@@ -1,6 +1,5 @@
 package app.ownplay.player.ui
 
-import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
@@ -11,10 +10,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameNanos
-import androidx.compose.ui.focus.FocusDirection
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalFocusManager
 import app.ownplay.player.OwnPlayAppRuntime
 
 @Composable
@@ -26,23 +21,9 @@ fun OwnPlayRoot(
     onLivePreviewActiveChanged: (Boolean) -> Unit = {},
 ) {
     var contentVisible by remember { mutableStateOf(false) }
-    val configuration = LocalConfiguration.current
-    val focusManager = LocalFocusManager.current
-    val isTelevision =
-        configuration.uiMode and Configuration.UI_MODE_TYPE_MASK == Configuration.UI_MODE_TYPE_TELEVISION
 
     LaunchedEffect(Unit) {
         contentVisible = true
-    }
-
-    LaunchedEffect(isTelevision, contentVisible) {
-        if (isTelevision && contentVisible) {
-            withFrameNanos { }
-            if (!focusManager.moveFocus(FocusDirection.Next)) {
-                withFrameNanos { }
-                focusManager.moveFocus(FocusDirection.Next)
-            }
-        }
     }
 
     AnimatedVisibility(
@@ -53,21 +34,12 @@ fun OwnPlayRoot(
                 initialScale = 0.985f,
             ),
     ) {
-        if (isTelevision) {
-            OwnPlayTvApp(
-                runtime = runtime,
-                onPlaybackFullscreenChanged = onPlaybackFullscreenChanged,
-                onPlaybackSurfaceActiveChanged = onPlaybackSurfaceActiveChanged,
-                onLivePreviewActiveChanged = onLivePreviewActiveChanged,
-            )
-        } else {
-            OwnPlayApp(
-                runtime = runtime,
-                rotationFullscreenEnabled = rotationFullscreenEnabled,
-                onPlaybackFullscreenChanged = onPlaybackFullscreenChanged,
-                onPlaybackSurfaceActiveChanged = onPlaybackSurfaceActiveChanged,
-                onLivePreviewActiveChanged = onLivePreviewActiveChanged,
-            )
-        }
+        PlatformOwnPlayRoot(
+            runtime = runtime,
+            rotationFullscreenEnabled = rotationFullscreenEnabled,
+            onPlaybackFullscreenChanged = onPlaybackFullscreenChanged,
+            onPlaybackSurfaceActiveChanged = onPlaybackSurfaceActiveChanged,
+            onLivePreviewActiveChanged = onLivePreviewActiveChanged,
+        )
     }
 }
