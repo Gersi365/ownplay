2db042ea82bd47bf088109a745809b4f1165756e
Gersi365 <Gersi365@outlook.com>
2026-08-30 08:57:36 +0200

    Define Mobile build target policy

---
+++ app/src/mobile/java/app/ownplay/player/target/OwnPlayBuildTarget.kt
@@ -0,0 +1,15 @@
+package app.ownplay.player.target
+
+import app.ownplay.player.personalization.AppDeviceProfile
+
+internal object OwnPlayBuildTarget {
+    const val usesDpad: Boolean = false
+    const val supportsTouchInput: Boolean = true
+    const val supportsPictureInPicture: Boolean = true
+
+    val fixedProfile: AppDeviceProfile? = null
+    val selectableProfiles: Set<AppDeviceProfile> = setOf(
+        AppDeviceProfile.SMARTPHONE,
+        AppDeviceProfile.TABLET,
+    )
+}
