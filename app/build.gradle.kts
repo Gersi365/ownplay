c31d83691df18925cd7e36661efde0412ccc880e
Gersi365 <Gersi365@outlook.com>
2026-09-02 17:07:45 +0200

    Bump recoverable multi-source QA update to v7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.3-m3u-update"
+        versionCode = 7
+        versionName = "1.0.5-multisource-update"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
