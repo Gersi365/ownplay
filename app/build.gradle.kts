bd2f2f4268af2991728c0a531f727e06e1c4f86f
Gersi365 <Gersi365@outlook.com>
2026-09-02 10:36:28 +0200

    Bump M3U update build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1-m3u-update"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
