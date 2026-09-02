30b37dee5da7fbb3d5b2b3edfcf09f7d9c4cd00e
Gersi365 <Gersi365@outlook.com>
2026-09-02 15:29:05 +0200

    Build hardened active-playlist update APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.3-m3u-update"
+        versionCode = 6
+        versionName = "1.0.4-m3u-update"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
